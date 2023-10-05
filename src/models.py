c3c8d10eecb8ac12ff97d4df43596b5fe64aab6b
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-10-05 23:37:55 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,37 +1,71 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
+    ID = Column(Integer, primary_key=True)
+    username = Column(String, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    posts = relationship('Post', back_populates='user')
+    followers_from = relationship('Follower', foreign_keys='Follower.user_from_id', back_populates='user_from')
+    followers_to = relationship('Follower', foreign_keys='Follower.user_to_id', back_populates='user_to')
+    comments = relationship('Comment', back_populates='author')
 
-    def to_dict(self):
-        return {}
+class Post(Base):
+    __tablename__ = 'posts'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.ID'))
+    
+    user = relationship('User', back_populates='posts')
+    media = relationship('Media', back_populates='post')
+    comments = relationship('Comment', back_populates='post')
+
+class Media(Base):
+    __tablename__ = 'media'
+    ID = Column(Integer, primary_key=True)
+    type = Column(Enum('image', 'video', 'audio'))
+    url = Column(String)
+    post_id = Column(Integer, ForeignKey('posts.ID'))
+
+    post = relationship('Post', back_populates='media')
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    user_from_id = Column(Integer, ForeignKey('users.ID'), primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('users.ID'), primary_key=True)
+
+    user_from = relationship('User', back_populates='followers_from')
+    user_to = relationship('User', back_populates='followers_to')
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    ID = Column(Integer, primary_key=True)
+    comment_text = Column(String)
+    author_id = Column(Integer, ForeignKey('users.ID'))
+    post_id = Column(Integer, ForeignKey('posts.ID'))
+
+    author = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+
+# Crea el motor de la base de datos
+engine = create_engine('sqlite:///social_media.db')
+Base.metadata.create_all(engine)
 
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem generating the diagram")
     raise e
+
